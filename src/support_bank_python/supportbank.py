4e283ab75ed682bb74ea5bf0394be5c08ee177ee
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2025-09-30 14:25:41 +0100

    Fix: function and variable naming - changed all to snake case

--- src/support_bank_python/supportbank.py
+++ src/support_bank_python/supportbank.py
@@ -17,54 +17,54 @@ class Account:
 
 
 class Transaction:
-    def __init__(self, date, fromAcc, toAcc, narrative, amount):
+    def __init__(self, date, from_acc, to_acc, narrative, amount):
         self.date = date
-        self.fromAcc = fromAcc
-        self.toAcc = toAcc
+        self.from_acc = from_acc
+        self.to_acc = to_acc
         self.narrative = narrative
         self.amount = amount
 
-def poundsToPence(pounds):
+def pounds_to_pence(pounds):
     return round(pounds*100, 0)
 
-def penceToPounds(pence):
+def pence_to_pounds(pence):
     return round(pence/100, 2)
 
-def readfile(filePath, accountLedger, transactionLedger):
-    with open(filePath, mode='r') as file:
-        csvFile = csv.DictReader(file)
-        for line in csvFile:
-            amount = poundsToPence(float(line['Amount']))
+def read_file(file_path, account_ledger, transaction_ledger):
+    with open(file_path, mode='r') as file:
+        csv_file = csv.DictReader(file)
+        for line in csv_file:
+            amount = pounds_to_pence(float(line['Amount']))
             t = Transaction(line['Date'], line['From'], line['To'], line['Narrative'], amount)
-            transactionLedger.append(t)
-            if line['From'] not in accountLedger:
-                accountLedger[line['From']] = Account(line['From'])
-            if line['To'] not in accountLedger:
-                accountLedger[line['To']] = Account(line['To'])
-            accountLedger[line['From']].update_balance_for_from(amount)
-            accountLedger[line['To']].update_balance_for_to(amount)
-            accountLedger[line['From']].update_transactions(t)
-            accountLedger[line['To']].update_transactions(t)
+            transaction_ledger.append(t)
+            if line['From'] not in account_ledger:
+                account_ledger[line['From']] = Account(line['From'])
+            if line['To'] not in account_ledger:
+                account_ledger[line['To']] = Account(line['To'])
+            account_ledger[line['From']].update_balance_for_from(amount)
+            account_ledger[line['To']].update_balance_for_to(amount)
+            account_ledger[line['From']].update_transactions(t)
+            account_ledger[line['To']].update_transactions(t)
 
-def listAll(accountLedger):
-    for k, v in accountLedger.items():
-        print(k, penceToPounds(v.balance))
+def list_all(account_ledger):
+    for k, v in account_ledger.items():
+        print(k, pence_to_pounds(v.balance))
 
-def listAccount(accountLedger, account):
-    accountTransactions = accountLedger[account].transactions
-    for t in accountTransactions:
-        print(t.date, t.fromAcc, t.toAcc, t.narrative, penceToPounds(t.amount))
+def list_account(account_ledger, account):
+    account_transactions = account_ledger[account].transactions
+    for t in account_transactions:
+        print(t.date, t.from_acc, t.to_acc, t.narrative, pence_to_pounds(t.amount))
 
 def main():
-    accountLedger = dict()
-    transactionLedger = []
-    readfile(r"C:\Users\taskur\Work\Training\PythonProjects\Support-Bank-Python\data\Transactions2014.csv", accountLedger, transactionLedger)
+    account_ledger = dict()
+    transaction_ledger = []
+    read_file(r"C:\Users\taskur\Work\Training\PythonProjects\Support-Bank-Python\data\Transactions2014.csv", account_ledger, transaction_ledger)
     command = input("Enter command: ")
     if command == 'List All':
-        listAll(accountLedger)
+        list_all(account_ledger)
     elif command[:4] == 'List':
         account = command[5:]
-        listAccount(accountLedger, account)
+        list_account(account_ledger, account)
     else:
         print("Wrong command!")
 
